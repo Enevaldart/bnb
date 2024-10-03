ac41b704a845078f5086d513615ef90f57ef7cda
Enevald <enevaldbravo80@gmail.com>
2024-10-03 09:52:40 +0300

    Added the active nav tab

--- app/ui/dashNavbar/navbar.tsx
+++ app/ui/dashNavbar/navbar.tsx
@@ -1,37 +1,32 @@
 "use client";
 
-import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import styles from './navbar.module.css';
 
 const Navbar = () => {
-  const [activeTab, setActiveTab] = useState('employee-list');
-
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-  };
+  const pathname = usePathname();
 
   return (
     <div className={styles.navbar}>
       <div className={styles.tabs}>
-          <a
-            className={activeTab === 'employee-list' ? styles.active : ''}
-            onClick={() => handleTabClick('employee-list')} href="/dashboard/employee-list"
-          >
-            Booking List
-          </a>
-          <a
-            className={activeTab === 'myHomes' ? styles.active : ''}
-            onClick={() => handleTabClick('myHomes')} href="/dashboard/myHomes"
-          >
-            My Homes
-          </a>
-          <a
-            className={activeTab === 'others' ? styles.active : ''}
-            onClick={() => handleTabClick('others')} href="/dashboard/others"
-          >
-            Others
-          </a>
+        <a
+          className={pathname === '/dashboard' ? styles.active : ''}
+          href="/dashboard"
+        >
+          Booking List
+        </a>
+        <a
+          className={pathname === '/dashboard/myHomes' ? styles.active : ''}
+          href="/dashboard/myHomes"
+        >
+          My Homes
+        </a>
+        <a
+          className={pathname === '/dashboard/others' ? styles.active : ''}
+          href="/dashboard/others"
+        >
+          Others
+        </a>
       </div>
       <div className={styles.addButton}>
         <button className={styles.addHome}>+ Add Home</button>
