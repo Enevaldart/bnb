bb94c62ab5867c7442baa436f069efc2809ac732
Enevald <enevaldbravo80@gmail.com>
2024-10-03 12:47:12 +0300

    fixed the navbar to the top of the page

--- app/homes/layout.tsx
+++ app/homes/layout.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import Image from "next/image";
-import ResponsiveSearch from '../ui/navbarHomes/navbar';
-import  "./globals.css"
+import ResponsiveSearch from "../ui/navbarHomes/navbar";
+import "./globals.css";
 
 interface AuthLayoutProps {
   children: ReactNode;
@@ -9,9 +9,14 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className='body' style={{ minHeight: '90vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', width: 'fit-content', margin: '0 auto' }}>
-      <ResponsiveSearch />
+    <div
+      className="body">
+      <div className="nav1">
+        <ResponsiveSearch />
+      </div>
+      <div className="children">
       {children}
+      </div>
     </div>
   );
 };
