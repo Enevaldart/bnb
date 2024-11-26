3030641b4f43c8e34dadc94c0a6869c488d8d7ad
Enevald <enevaldbravo80@gmail.com>
2024-11-26 18:36:44 +0300

    all necessary commits for the frontend landing page

--- app/ui/advancedSearch/advancedSearch.tsx
+++ app/ui/advancedSearch/advancedSearch.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./advancedSearch.module.css";
 import { CiFilter } from "react-icons/ci";
 
 const AdvancedSearch = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -14,56 +15,55 @@ const AdvancedSearch = () => {
   return (
     <div className={styles.container}>
       <button onClick={toggleExpand} className={styles.toggleButton}>
-        {isExpanded ? "Hide Advanced Search" : "Click here for Advanced Search"}
+        {isExpanded
+          ? "Click here for Advanced Search"
+          : "Click here for Advanced Search"}
         <CiFilter />
       </button>
 
       <form className={styles.form}>
-        {isExpanded && (
-          <>
+        <div
+          ref={contentRef}
+          className={`${styles.content} ${isExpanded ? styles.expanded : ""}`}
+        >
+          <div className={styles.fieldGroup}>
+            <label htmlFor="location">Location</label>
+            <input type="text" id="location" placeholder="Enter location" />
+          </div>
+
+          <div className={styles.fieldGroup}>
+            <label htmlFor="priceRange">Budget</label>
+            <div className={styles.rangeInputs}>
+              <input type="number" placeholder="Min price" />
+              <input type="number" placeholder="Max price" />
+            </div>
+          </div>
+          <div className={styles.row}>
             <div className={styles.fieldGroup}>
-              <label htmlFor="location">Location</label>
-              <input type="text" id="location" placeholder="Enter location" />
+              <label htmlFor="propertyType">Property Type</label>
+              <select id="propertyType">
+                <option value="any">Any</option>
+                <option value="apartment">Hotel</option>
+                <option value="villa">Villa</option>
+                <option value="studio">Studio</option>
+              </select>
             </div>
 
             <div className={styles.fieldGroup}>
-              <label htmlFor="priceRange">Budget</label>
-              <div className={styles.rangeInputs}>
-                <input type="number" placeholder="Min price" />
-                <input type="number" placeholder="Max price" />
-              </div>
-            </div>
-            <div className={styles.row}>
-              <div className={styles.fieldGroup}>
-                <label htmlFor="propertyType">Property Type</label>
-                <select id="propertyType">
-                  <option value="any">Any</option>
-                  <option value="apartment">Hotel</option>
-                  <option value="villa">Villa</option>
-                  <option value="studio">Studio</option>
-                </select>
-              </div>
-
-              <div className={styles.fieldGroup}>
-                <label htmlFor="bedrooms">Bedrooms</label>
-                <input
-                  type="number"
-                  id="bedrooms"
-                  placeholder="Number of bedrooms"
-                />
-              </div>
-            </div>
-
-            <div className={styles.fieldGroup}>
-              <label htmlFor="keywords">Keywords</label>
+              <label htmlFor="bedrooms">Bedrooms</label>
               <input
-                type="text"
-                id="keywords"
-                placeholder="E.g., pool, garden"
+                type="number"
+                id="bedrooms"
+                placeholder="Number of bedrooms"
               />
             </div>
-          </>
-        )}
+          </div>
+
+          <div className={styles.fieldGroup}>
+            <label htmlFor="keywords">Keywords</label>
+            <input type="text" id="keywords" placeholder="E.g., pool, garden" />
+          </div>
+        </div>
         <button type="submit" className={styles.searchButton}>
           Search
         </button>
